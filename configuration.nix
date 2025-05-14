2524472f555f691b8cc55cb3e0fb57ef28daa78b
Aurelio Florez <aurelio@florezdelvalle.com>
2025-05-13 22:32:31 -0400

    it works! - missing wifi card

--- configuration.nix
+++ configuration.nix
@@ -56,15 +56,6 @@
   # Enable Tailscale
   services.tailscale.enable = true;
 
-  # Nginx Reverse Proxy
-
-  services.nginx = {
-	  enable = true;
-	  virtualHosts."base.org.es" = {
-    		locations."/".proxyPass = "http://127.0.0.1:80";
-	  };
-  };
-
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
